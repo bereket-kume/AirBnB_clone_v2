d0453255961dd78da3a421a9943d545704bacad1
bereket kume <bereketkume@gmail.com>
2024-04-17 12:06:26 -0700

    done

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Create or use the database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or use the user hbnb_dev with password hbnb_dev_pwd
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the database hbnb_dev_db to the user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the database performance_schema to the user hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
