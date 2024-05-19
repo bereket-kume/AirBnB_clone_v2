51f9ba342425c48abb94abc207442d464f4e61dd
bereket kume <bereketkume@gmail.com>
2024-05-19 06:23:25 -0700

    done

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Flask web application that routing """
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/state_list', strict_slashes=False)
+def state_list():
+    """ display state list in order """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states-states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ close connection between storage """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
