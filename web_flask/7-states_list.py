898948bf98e594788a0b6d9475896d75f45e99e1
bereket kume <bereketkume@gmail.com>
2024-05-19 06:36:13 -0700

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,12 +12,14 @@ app = Flask(__name__)
 def state_list():
     """ display state list in order """
     states = storage.all('State')
-    return render_template('7-states_list.html', states-states)
+    return render_template('7-states_list.html', states - states)
+
 
 @app.teardown_appcontext
 def teardown(exc):
     """ close connection between storage """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
